512ace86dc806c72a825cb7264d68fe0b63545b8
alexanderHandstanger <zombeyfan.ah@gmail.com>
2019-01-08 16:44:18 +0000

    fixes error with add_pack_animal; 9 tests run successfully

--- caravan.cpp
+++ caravan.cpp
@@ -40,12 +40,38 @@ int get_length(Caravan caravan)
 
 void delete_caravan(Caravan caravan)
 {
-
+  Node current = caravan->head;
+  while(current != 0){
+    Node should_delete = current;
+    current = current->next;
+    sfree(should_delete);
+  }
+  sfree(caravan);
 }
 
 void add_pack_animal(Caravan caravan, PackAnimal animal)
 {
-
+  if(animal == 0){
+    return;
+  }
+  Node current = caravan->head;
+  Node to_add = (Node) malloc(sizeof(struct NodeImplementation));
+  to_add->animal = animal;
+  to_add->next = 0;
+  if(caravan->head == 0){
+    caravan->head = to_add;
+  }
+  else{
+    while(current->next != 0){
+      current = current->next;
+    }
+    if(current->animal == animal){
+      return;
+    }
+    current->next = to_add;
+  }
+  add_to_caravan(animal, caravan);
+  caravan->length++;
 }
 
 void remove_pack_animal(Caravan caravan, PackAnimal animal)
